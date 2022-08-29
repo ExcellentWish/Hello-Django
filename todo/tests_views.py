18dfa4d0b1c651403484454b37a4477146ecf9ef
Nigel Doheny <nigel.doheny93@gmail.com>
2022-08-29 22:31:09 +0000

    Start testing

--- todo/tests_views.py
+++ todo/tests_views.py
@@ -5,13 +5,4 @@ class TestDjango(TestCase):
 
     def test_this_thing_works(self):
         self.assertEqual(1, 1)
-
-    def test_this_thing_work1(self):
-        self.assertEqual(1, 1)
-
-    def test_this_thing_works2(self):
-        self.assertEqual(1, 1)  
-
-
-    def test_this_thing_works3(self):
-        self.assertEqual(1, 1)             
+          
